0d44b31a52f1ad037b511f07cc361607e1bbe4b7
Atsushi Nakatsugawa <atsushi@moongift.jp>
2018-05-24 09:18:54 +0900

    Update

---
+++ app.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+  console.log("Hello, Isaax. I'm Raspberry Pi!");
+  console.log(`My env is ${process.env.MY_ENV}`);
+  console.log('Updated')
+}, 5000);
--- app.rb
+++
@@ -1,5 +0,0 @@
-STDOUT.sync = true
-while true do
-  puts "こんにちは Isaax, 私は Raspberry Piです。"
-  sleep(20)
-end
