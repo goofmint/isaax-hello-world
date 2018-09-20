35bf7a58769881fa3b6354855c96ac6ac98f0a26
Atsushi <atsushi@moongift.jp>
2018-09-20 16:28:55 +0900

    Update version

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import "time"
 func main() {
   for {
       // ここに無限ループで実行する処理を記述
-      fmt.Println("Hello isaax.")
+      fmt.Println("Hello isaax. I'm Go!")
       time.Sleep(5 * time.Second)
   }
 }
